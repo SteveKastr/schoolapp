878e4dacb5f392aa1e2e1c9e3b5e650aa6d4312c
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-07 21:44:55 +0300

    feat: add repositories with entity-graph

--- src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
@@ -3,6 +3,7 @@ package gr.aueb.cf.schoolapp.repository;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,6 +17,7 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     Optional<Teacher> findByUuid(UUID uuid);
 
 
+    @EntityGraph(attributePaths = {"region"})
     Page<Teacher> findAllByDeletedFalse(Pageable pageable);
     Optional<Teacher> findByVatAndDeletedFalse(String vat);
     Optional<Teacher> findByUuidAndDeletedFalse(UUID uuid);
