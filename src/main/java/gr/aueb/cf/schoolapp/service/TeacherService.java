098becc5c59df2b2f1084e89f5126e11107f7573
Stefanos Kastritsis <kasstefan@hotmail.com>
2026-05-07 15:33:26 +0300

    chore: add delete logic

--- src/main/java/gr/aueb/cf/schoolapp/service/TeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/TeacherService.java
@@ -35,6 +35,13 @@ public class TeacherService implements ITeacherService {
         return teachersPage.map(mapper::mapToTeacherReadOnlyDTO);
     }
 
+    @Override
+    public Page<TeacherReadOnlyDTO> getPaginatedTeachersDeletedFalse(Pageable pageable) {
+        Page<Teacher> teachersPage = teacherRepository.findAllByDeletedFalse(pageable);
+        log.debug("Get paginated not deleted returned successfully page={} and size={}", teachersPage.getNumber(), teachersPage.getSize());
+        return teachersPage.map(mapper::mapToTeacherReadOnlyDTO);
+    }
+
     private final TeacherRepository teacherRepository;
     private final RegionRepository regionRepository;
     private final Mapper mapper;
@@ -120,6 +127,23 @@ public class TeacherService implements ITeacherService {
             throw e;
         }  catch (EntityInvalidArgumentException e) {
             log.error("Update failed for teacher with uuid={}. Region id={} invalid.", dto.uuid(), dto.regionId(), e);
+            throw e;
+        }
+    }
+
+    @Override
+    @Transactional(rollbackFor = EntityNotFoundException.class)
+    public TeacherReadOnlyDTO deleteTeacherByUUID(UUID uuid) throws EntityNotFoundException {
+        try {
+
+            Teacher teacher = teacherRepository.findByUuidAndDeletedFalse(uuid)
+                    .orElseThrow(() -> new EntityNotFoundException("Teacher with uuid= " + uuid + " not found."));
+            teacher.softDelete();
+            log.info("Teacher with uuid={} deleted successfully.", uuid);
+            return mapper.mapToTeacherReadOnlyDTO(teacher);
+
+        } catch (EntityNotFoundException e) {
+            log.error("Update failed for teacher with uuid={}. Teacher not found", uuid, e);
             throw e;
         }
     }
